2713103f1b2bf6da96b7763bed64f1048cb2fecd
abibrito <abinadabebrito@gmail.com>
2023-06-28 13:20:57 -0300

    feat: Função de remover Loading

--- js/scriptForms.js
+++ js/scriptForms.js
@@ -1,6 +1,10 @@
+const button = document.querySelector('button');
 const loading = () => {
-  const button = document.querySelector('button');
-button.innerHTML = '<img src="./img/loading.png" class="loading" />';
+  button.innerHTML = '<img src="./img/loading.png" class="loading" />';
+}
+
+const removeLoading = () => {
+  button.innerHTML = 'Fale Conosco';
 }
 
 const funcSubmit = (event) => {
@@ -12,14 +16,13 @@ const funcSubmit = (event) => {
 
 
   fetch('https://api.sheetmonkey.io/form/q1WcQzWoNrG7TrkiSZU75D', {
-
     method: 'post',
     headers: {
       'Accept': 'application/json',
       'Content-Type': 'application/json',
     },
     body: JSON.stringify({ name, email }),
-  });
+  }).then(() => removeLoading());
 }
 
 document.querySelector('form').addEventListener('submit', funcSubmit);
